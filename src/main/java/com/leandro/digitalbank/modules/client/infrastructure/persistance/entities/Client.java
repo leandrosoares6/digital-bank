335a892324e2a98ba322a0d70efc17505f2b539a
Leandro Neves <leandrosoares6@hotmail.com>
2020-10-20 17:48:41 -0300

    Flyway added to create migrations

--- src/main/java/com/leandro/digitalbank/modules/client/infrastructure/persistance/entities/Client.java
+++ src/main/java/com/leandro/digitalbank/modules/client/infrastructure/persistance/entities/Client.java
@@ -1,47 +1,32 @@
 package com.leandro.digitalbank.modules.client.infrastructure.persistance.entities;
 
-public class Client {
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Client implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue
 	private Long id;
 	private String fisrtName;
 	private String lastName;
 	private String email;
 	private String dateOfBirth;
 	private String cpf;
-	
-	public Long getId() {
-		return id;
-	}
-	public void setId(Long id) {
-		this.id = id;
-	}
-	public String getFisrtName() {
-		return fisrtName;
-	}
-	public void setFisrtName(String fisrtName) {
-		this.fisrtName = fisrtName;
-	}
-	public String getLastName() {
-		return lastName;
-	}
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	public String getDateOfBirth() {
-		return dateOfBirth;
-	}
-	public void setDateOfBirth(String dateOfBirth) {
-		this.dateOfBirth = dateOfBirth;
-	}
-	public String getCpf() {
-		return cpf;
-	}
-	public void setCpf(String cpf) {
-		this.cpf = cpf;
-	}
 }
